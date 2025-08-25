f7cad36f8c60a53b3bbc6880ef4246c90427d3ec
FELIPE <felipe_carli@hotmail.com>
2025-08-25 19:36:47 -0300

    feat: cria CRUD de Brand com controller e service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { CategoryModule } from './cases/categories/category.module';
+import { BrandModule } from './cases/brands/brand.module';
 
 @Module({
   imports: [
@@ -8,12 +9,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       host: 'aws-0-sa-east-1.pooler.supabase.com',
       port: +'5432',
-      username: 'postgres.szafiixsxvgvggmzgawn',
+      username: 'postgres.zriqiljmquzhjussbqde',
       password: 'postgres',
       database: 'postgres',
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+    CategoryModule,
+    BrandModule
   ],
 })
 export class AppModule {}
